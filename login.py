44e1df9bf37cf0c1a10e2be5b1e96076ae45fe68
Faizan Ahmad <faizanahmad33.fa@gmail.com>
2018-03-24 06:31:08 +0500

    Exceptional handling and first time login error fixed

--- login.py
+++ login.py
@@ -6,15 +6,18 @@ import re
 user_name = ""   								#global declaration of user_name,filename,d(dictionary)
 filename = ""
 d = {}
-directory = "Data"	#Path for file
-name = "usersdata.txt"						#name of file
-filename = os.path.join(directory, name)	#joining directory with file for further use
 
+#relative path for file
+directory = "Data"
+name = "usersdata.txt"
+filename = os.path.join(directory, name)
 
 def login_user():
     global d 								#main funtion which calls further funtions,execution starts from here
-    data()										#data funtion is called to check or make changes in it
-    user = input("Please Select One \n1. Login \n2. Create New Account \n0. Exit \n")
+    data()                                  #data funtion is called to check or make changes in it
+    user = input("Select One : \n1. Login \n2. Create New Account \n0. Exit \n")
+    os.system('cls' if os.name == 'nt' else 'clear')
+
     if not str(user).isdigit():
         print ("Invalid Selection!")
         return login_user()
@@ -22,11 +25,11 @@ def login_user():
         login()									#login function called for further execution
 
     elif int(user) == 2:
+        os.system('cls' if os.name == 'nt' else 'clear')
         new_account()							#new_account function called for further execution
 
-    elif int(user) == 0:
+    elif int(user) == 0:                        #exits the main funtion
         print ("Good Bye!")
-    											#exits the main funtion
 
     else:
         print ("Invalid Selection!")
@@ -37,6 +40,7 @@ def login_user():
 def data():										#when 1 is entered from main(login_user)
     global filename,name
     global d
+
     try:
         with open(filename, "a+") as ap:
             #file size shorter than 13 bit
@@ -44,13 +48,14 @@ def data():										#when 1 is entered from main(login_user)
                 ap.write('abc xyz:1234,0.0')
                 ap.close()
                 print ("Please create an account first!")
-                return
+                return login_user()
+
             else:
                 with open(filename, "r+") as rd:
                     id_user = rd.read().split("\n")				#opened file in data read mode
                     for i in id_user:
                         a = re.split("[:,]",i)
-                        a[1],a[2] = int(a[1]),float(a[2])
+                        a[1],a[2] = str(a[1]),float(a[2])
                         d[a[0]] = a[1],a[2]
                     return d
     except:
@@ -61,63 +66,100 @@ def data():										#when 1 is entered from main(login_user)
 
 def login():
     global user_name
-    user_name = input("Name : ")
+    os.system('cls' if os.name == 'nt' else 'clear')
+    user_name = input("Login\nName : ")
     entry = 0
+
     if user_name in d.keys():
         while int(entry) != 3:
-            pin = int(input("Enter 4-Digit Pin : "))
+            print("Entries left :",(3-entry))
+            pin = str(input("Enter 4-Digit Pin : "))
+
             if pin == d[user_name][0]:
                 Net_balance = d[user_name][1]
                 Pin = d[user_name][0]
+                os.system('cls' if os.name == 'nt' else 'clear')
                 return atm(user_name,Net_balance,Pin)
+
             else:
                 entry += 1
+                os.system('cls' if os.name == 'nt' else 'clear')
                 print ("Incorrect Pin")
-        print ("Login Unsuccessful")
+        os.system('cls' if os.name == 'nt' else 'clear')
+        print ("Login Unsuccessful\n")
         return login_user()
+
     else:
+        os.system('cls' if os.name == 'nt' else 'clear')
         print ("Invalid User")
         return login_user()
 
 
 def new_account():
-    user_name1 = input("Enter First Name : ")
+    user_name1 = input("New Account\nEnter First Name : ")
+    os.system('cls' if os.name == 'nt' else 'clear')
     user_name2 = input("Enter Last Name : ")
 
-    if (user_name1.isalpha() == False) or (user_name2.isalpha() == False):
+    if (user_name1.isalpha() == False) or (user_name2.isalpha() == False) or (user_name1 == user_name2):
+        os.system('cls' if os.name == 'nt' else 'clear')
         print ("Invalid Name")
         return new_account()
+
+    os.system('cls' if os.name == 'nt' else 'clear')
     pin_count = 0
     while pin_count != 3:
+        print("Entries left :",(3-pin_count))
         pin = str(input ("Enter 4-Digit Pin : "))
+        os.system('cls' if os.name == 'nt' else 'clear')
+
         if (len(pin) == 4) and (pin.isdigit() == True):
+            os.system('cls' if os.name == 'nt' else 'clear')
             confirm_pin = str(input ("Confirm Pin : "))
+
             if pin == confirm_pin:
+                os.system('cls' if os.name == 'nt' else 'clear')
                 print ("Account Name :",user_name1+' '+user_name2,"\nPin :",pin)
                 confirm = input("Please Confirm \n1. Yes \n2. No \n")
 
                 if (confirm == '1') or (confirm.lower().startswith('y')):
+                    os.system('cls' if os.name == 'nt' else 'clear')
                     with open(filename, "a") as wr:
                         new = "\n"+user_name1+' '+user_name2+":"+pin+",0.0"
                         wr.write(new)
                         wr.close()
                         print ("Account Created Successfully! \n")
                         return login_user()
+
                 elif (confirm == '2') or (confirm.lower().startswith('n')):
-                    return new_account()
+                    os.system('cls' if os.name == 'nt' else 'clear')
+                    print ("Account Not Created!")
+                    return login_user()
+
                 else:
+                    os.system('cls' if os.name == 'nt' else 'clear')
+                    print ("Account Not Created!")
                     return new_account()
 
             else:
                 print ("Your Pin Did Not Match!")
                 pin_count +=1
+
         else:
-            pin_count = 0
+            pin_count = pin_count
+            os.system('cls' if os.name == 'nt' else 'clear')
+            print ("Invalid Pin")
+
+    os.system('cls' if os.name == 'nt' else 'clear')
     print ("Account Not Created!")
-    return login()
+    return login_user()
+
+
 
 try:
+    os.system('cls' if os.name == 'nt' else 'clear')
     login_user()
 except:
     Exception
-    print ("Some errors came encountered,\nPlease be careful next time.\nGood bye!")
+    os.system('cls' if os.name == 'nt' else 'clear')
+    print ("Sorry for inconvenience.")
+    print ("Some errors were encountered,\nPlease be careful next time.\nGood bye!")
