86e33b78c841a3e1522a5c38d5a1e30f13bcf121
Faizan Ahmad <faizanahmad33.fa@gmail.com>
2018-03-27 02:14:43 +0500

    new file is created for filehandling

---
+++ Data.py
@@ -0,0 +1,38 @@
+from encrypt import rot13
+import os
+import re
+
+#relative path for file
+def join():
+    directory = "Data"
+    name = "usersdata.txt"
+    filename = os.path.join(directory, name)
+    return filename
+
+def data():										#when 1 is entered from main(login_user)
+    filename = join()
+    d = {}
+
+    try:
+        with open(filename, "a+") as ap:
+            #file size shorter than 13 bit
+            if os.stat(filename).st_size <= 0:
+                ap.write('nop klm:1234,0.0')
+                ap.close()
+                print ("Please create an account first!")
+                return
+
+            else:
+                with open(filename, "r+") as rd:
+                    id_user = rd.read().split("\n")				#opened file in data read mode
+                    for items in id_user:
+                        indiv_user_info = re.split("[:,]",items)
+                        #rot13() function is called for decoding
+                        indiv_user_info[0] = rot13(indiv_user_info[0])
+                        indiv_user_info[1],indiv_user_info[2] = str(indiv_user_info[1]),float(indiv_user_info[2])
+                        d[indiv_user_info[0]] = indiv_user_info[1],indiv_user_info[2]
+                    return d
+    except:
+        IOError or FileNotFoundError
+        os.mkdir("Data")
+        data()
