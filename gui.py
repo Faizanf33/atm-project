c869886924840e3829733ccf613510d0fe61c1ab
Mr <p176053@nu.edu.pk>
2018-03-27 21:07:13 +0500

    Starting GUI

---
+++ gui.py
@@ -0,0 +1,20 @@
+########################
+# GUI-Author: Nauman Afsar
+# Date: 27-03-2018
+#########################
+
+#########################
+# importing Libraries
+#########################
+import tkinter as tk        # Python 3: "t" lower-case
+
+# Create instance
+win = tk.Tk()
+
+# Add a title
+win.title("ATM Project")
+
+########################
+# Start GUI
+#######################
+win.mainloop()
