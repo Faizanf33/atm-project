a24bb9f5d57703d15f4403ce18121ffe795135c1
Mr <p176053@nu.edu.pk>
2018-03-27 21:38:59 +0500

    Fixed the windows size

--- gui.py
+++ gui.py
@@ -8,13 +8,25 @@
 #########################
 import tkinter as tk        # Python 3: "t" lower-case
 
-# Create instance
-win = tk.Tk()
 
-# Add a title
+win = tk.Tk()
 win.title("ATM Project")
 
-########################
+def windows_size():
+    win.update()                        # to get runtime size
+    print('width  =', win.winfo_width())
+    print('height =', win.winfo_height())
+
+def increase_window_width():
+    win.minsize(width=500, height=500)
+    # disable resizing the GUI
+    win.resizable(0,0)
+
+#======================
 # Start GUI
-#######################
+#======================
+windows_size()
+increase_window_width()
+print()
+windows_size()
 win.mainloop()
