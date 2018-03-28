7e060e5454d48474a8debc3a6fb53a3835ffa08d
Faizan Ahmad <faizanahmad33.fa@gmail.com>
2018-03-29 01:36:11 +0500

    minor fixes

--- Data.py
+++ Data.py
@@ -9,12 +9,13 @@ def join():
     filename = os.path.join(directory, name)
     return filename
 
-def data():										#when 1 is entered from main(login_user)
+#when 1 is entered from main(login_user)
+def data():
     filename = join()
     d = {}
 
     try:
-        with open(filename, "a+") as ap:
+        with open(filename, "a") as ap:
             #file size shorter than 13 bit
             if os.stat(filename).st_size <= 0:
                 ap.write('nop klm:1234,0.0')
@@ -23,7 +24,7 @@ def data():										#when 1 is entered from main(login_user)
                 return
 
             else:
-                with open(filename, "r+") as rd:
+                with open(filename, "r") as rd:
                     id_user = rd.read().split("\n")				#opened file in data read mode
                     for items in id_user:
                         indiv_user_info = re.split("[:,]",items)
@@ -32,6 +33,7 @@ def data():										#when 1 is entered from main(login_user)
                         indiv_user_info[1],indiv_user_info[2] = str(indiv_user_info[1]),float(indiv_user_info[2])
                         d[indiv_user_info[0]] = indiv_user_info[1],indiv_user_info[2]
                     return d
+
     except:
         IOError or FileNotFoundError
         os.mkdir("Data")
