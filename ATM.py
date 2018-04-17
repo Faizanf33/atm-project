bae3fcfc7eeb1c0636513904c90cf5aaf1a5f6e2
Faizan Ahmad <faizanahmad33.fa@gmail.com>
2018-04-17 13:53:18 +0500

    Option added to change pin

--- ATM.py
+++ ATM.py
@@ -18,6 +18,49 @@ except:
 #Atm function called after successfull login
 def atm(user_name,Net_balance,Pin,History):
     filename = join()
+    clear = ('cls' if os.name == 'nt' else 'clear')
+    #input for change of pin
+    new_pin_opt = input("Change Pin : \n1. Yes \n2. No \n")
+    os.system(clear)
+    if (new_pin_opt == '1') or (new_pin_opt.lower().startswith('y')):
+        os.system(clear)
+        pin_count = 0
+        print("Create Your Own Pin....")
+        while pin_count != 3:
+            print("Entries left :",(3-pin_count))
+            pin = str(input ("Enter 4-Digit Pin : "))
+            os.system(clear)
+
+            if (len(pin) == 4) and (pin.isdigit() == True):
+                if not pin == Pin:
+                    os.system(clear)
+                    confirm_pin = str(input ("Confirm Pin : "))
+
+                    if pin == confirm_pin:
+                        Pin = pin
+                        os.system(clear)
+                        print('Pin Changed Successfully! \n')
+                        pin_count = 3
+
+                    else:
+                        os.system(clear)
+                        print("Pin Change Unsuccessful!")
+                        print ("Your Pin Did Not Match! \n")
+                        pin_count +=1
+
+                else:
+                    pin_count += 1
+                    os.system(clear)
+                    print("Pin Change Unsuccessful!")
+                    print("Please Enter A New Pin \n")
+
+
+            else:
+                pin_count += 1
+                os.system(clear)
+                print("Pin Change Unsuccessful!")
+                print ("Invalid Pin! \n")
+
     import time,datetime
     print (time.strftime('Date:%d-%b-%Y \nTime:%I:%M %p  Today:%A\n'))
     print(("Dear"),user_name+("!"))
@@ -26,32 +69,33 @@ def atm(user_name,Net_balance,Pin,History):
     global net_balance
     net_balance += Net_balance
     Opr = input("Please Select An Option Provided Below : \n1. Check Account Balance \n2. Deposit \n3. Withdraw \n4. History \n0. Exit \n")
-    os.system('cls' if os.name == 'nt' else 'clear')
+    os.system(clear)
+
     if not Opr.isdigit():
-        return atm(user_name,Net_balance,Pin,History)
+        Opr = 5
+
     while int(Opr) != 0:
 
-        #Prints amount in counter
         if int(Opr) == 1:
-            os.system('cls' if os.name == 'nt' else 'clear')
+            os.system(clear)
             print ("Your Acount Balance = Rs","{:,}".format(net_balance),"\n")
 
         #Deposit function is called
         elif int(Opr) == 2:
-            os.system('cls' if os.name == 'nt' else 'clear')
+            os.system(clear)
             deposit(net_balance)
 
         #Withdraw function is called
         elif int(Opr) == 3:
-            os.system('cls' if os.name == 'nt' else 'clear')
+            os.system(clear)
             withdraw(net_balance)
 
         elif int(Opr) == 4:
-            os.system('cls' if os.name == 'nt' else 'clear')
+            os.system(clear)
             print ("Your Acount Was Previously Logged in on",History,"\n")
 
         else:
-            os.system('cls' if os.name == 'nt' else 'clear')
+            os.system(clear)
             print ("Wrong Option!")
 
         #Incase above condition(s) get meet
@@ -59,14 +103,14 @@ def atm(user_name,Net_balance,Pin,History):
         Opr = input("Please Select An Option Provided Below : \n1. Check Account Balance \n2. Deposit \n3. Withdraw \n4. History \n0. Exit \n")
         if not Opr.isdigit():
             Opr = 5
-            os.system('cls' if os.name == 'nt' else 'clear')
+            os.system(clear)
 
-    os.system('cls' if os.name == 'nt' else 'clear')
+    os.system(clear)
     print ("Thanks For Using ATM! \nWe Hope You Are Satisfied With Our Service.\nHave A Nice Day Ahead.")
 
     with open(filename,'a+') as ap:
         #rot13() function is called for encoding
-        enc = rot13(user_name)
+        enc = rot13(user_name.lower())
         re_new = [enc,str(Pin),str(net_balance),time.strftime('%d-%b-%Y at %I:%M %p')]
         w = csv.writer(ap)
         w.writerow(re_new)
@@ -75,6 +119,7 @@ def atm(user_name,Net_balance,Pin,History):
 
 #Deposit funtion starts when called by atm function
 def deposit(Net_balance):
+    clear = ('cls' if os.name == 'nt' else 'clear')
     global net_balance
     try:
         deposit_amount = input("Enter Amount In Rupees: ")
@@ -82,8 +127,9 @@ def deposit(Net_balance):
         #Check for negetive values
         if float(deposit_amount) >= 0:
             #check for extra large amount
+            #limits amount towards power of e
             if (len(deposit_amount) > 14) or ((len(str(float(deposit_amount)+net_balance))) > 14):
-                os.system('cls' if os.name == 'nt' else 'clear')
+                os.system(clear)
                 print ('Amount Limit Exceeded!')
                 return
 
@@ -94,22 +140,24 @@ def deposit(Net_balance):
                 return
 
         elif float(deposit_amount) < 0:
-            os.system('cls' if os.name == 'nt' else 'clear')
+            os.system(clear)
             #If user inputs negetive amount
             print ("Please Enter Right Amount! \n")
             return deposit(net_balance)
 
         else:
-            os.system('cls' if os.name == 'nt' else 'clear')
+            os.system(clear)
             print ("Please Enter Right Amount! \n")
             return deposit(net_balance)
 
     except ValueError:
-        os.system('cls' if os.name == 'nt' else 'clear')
+        os.system(clear)
         print ("Please Enter Right Amount! \n")
         return
+
 #deposit funtion starts when called by atm function
 def withdraw(Net_balance):
+    clear = ('cls' if os.name == 'nt' else 'clear')
     global net_balance
 
     #If amount is zero returns to atm function
@@ -120,11 +168,11 @@ def withdraw(Net_balance):
     else:
         try:
             with_draw = input("Enter Amount In Rupees: ")
-            os.system('cls' if os.name == 'nt' else 'clear')
+            os.system(clear)
 
             #If user inputs negetive amount
             if float(with_draw) < 0:
-                os.system('cls' if os.name == 'nt' else 'clear')
+                os.system(clear)
                 print ("Please Enter Right Amount! \n")
                 return withdraw(net_balance)
 
@@ -135,10 +183,10 @@ def withdraw(Net_balance):
                 return
 
             else:
-                os.system('cls' if os.name == 'nt' else 'clear')
+                os.system(clear)
                 print ("Withdrawl Impossible! \nYour Acount Balance = Rs",net_balance,"\n")
             return withdraw(net_balance)
         except ValueError:
-            os.system('cls' if os.name == 'nt' else 'clear')
+            os.system(clear)
             print ("Please Enter Right Amount! \n")
             return
