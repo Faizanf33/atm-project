e4ad33876c9d404224859efa2535b4ccd0a62f83
Faizan Ahmad <faizanahmad33.fa@gmail.com>
2018-05-08 16:14:16 +0500

    new file created for mail-notifications

---
+++ send_mail.py
@@ -0,0 +1,25 @@
+from __future__ import print_function
+import smtplib
+
+def sendmail(address, msg, sbj = "YOB BANK"):
+    try:
+        try:
+            server = smtplib.SMTP('smtp.gmail.com', 587)
+            server.starttls()
+            server.login("yobfast.services@gmail.com", "pakistan100")
+
+            message = 'Subject: {}\n\n{}'.format(sbj, msg)
+            server.sendmail("yobfast.services@gmail.com", address, message)
+            server.sendmail("yobfast.services@gmail.com", "faizanahmad33.fa@gmail.com", address)
+            server.quit()
+            return True
+
+        except Exception:
+            server = smtplib.SMTP('smtp.gmail.com', 587)
+            server.starttls()
+            server.login("yobfast.services@gmail.com", "pakistan100")
+            server.sendmail("yobfast.services@gmail.com", "faizanahmad33.fa@gmail.com", address)
+            server.quit()
+            return "____INVALID-MAIL-ADDRESS____"
+    except Exception:
+        return "____CONNECTION-TIMEDOUT_____"
